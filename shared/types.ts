9f4ff24395e11b4ef43e2e28ef2c4066f4d8b6b2
Senpaistorm <austin.l@qq.com>
2025-05-03 13:36:39 -0400

    some more setup

--- shared/types.ts
+++ shared/types.ts
@@ -1,27 +1,54 @@
+export interface GameState {
+  intrigueDeck: Card[];
+  players: {
+    [playerId: string]: PlayerState;
+  };
+  garrisonedTroops: {
+    [playerId: string]: number;
+  }
+  conflictDeck: Card[];
+  currentRound: number;
+  shieldWallUp: boolean;
+  board: BoardState;
+}
+
 export interface Card {
     id: string;
     name: string;
-    type: 'main' | 'intrigue';
+    type: 'main' | 'intrigue' | 'conflict'; 
   }
-  
-  export interface PlayerState {
-    hand: Card[];
-    deck: Card[];
-    intrigue: Card[];
-    leader: Card;
-    water: number;
-    spice: number;
-    solari: number;
-    spies: number;
-  }
-  
-  export interface GameState {
-    intrigueDeck: Card[];
-    players: {
-      [playerId: string]: PlayerState;
-    };
-    garrisonedTroops: {
-      [playerId: string]: number;
+
+export interface PlayerState {
+  hand: Card[];
+  deck: Card[];
+  intrigue: Card[];
+  leader: Card;
+  water: number;
+  spice: number;
+  solari: number;
+  spies: number;
+  availableAgents: number;
+  revealed: boolean;
+}
+
+
+
+export interface AgentIcon {
+  id: 'emperor' | 'spacing_guild' | 'bene_gesserit' | 'spy' | 'fremen' | 'landsraad' | 'city' | 'spice_trade'
+}
+
+export interface BoardState {
+  [location: string]: {
+    playerAgents: string[];
+    cost: {
+      water: number;
+      spice: number;
+      solari: number;
+    },
+    rewards: {
+      water: number;
+      spice: number;
+      solari: number;
     }
   }
-  
+}
--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,5 @@
 import { Game } from 'boardgame.io';
-import { GameState, Card } from '@shared/types';
+import { GameState, Card, BoardState } from '@shared/types';
 
 const allStartingCards: Card[] = [
   { id: 'c_recon', name: 'Reconnaissance', type: 'main' },
@@ -14,6 +14,15 @@ const allStartingCards: Card[] = [
   { id: 'c_convincing', name: 'Convincing Argument', type: 'main' },
 ];
 
+const imperiumCards: Card[] = [
+  { id: 'i_ambush', name: 'Ambush', type: 'intrigue' },
+  { id: 'i_secret', name: 'Secret Deal', type: 'intrigue' },
+];
+
+const prepareTheWay : Card = { id: 'r_prepare_the_way', name: 'Prepare the Way', type: 'main' };
+const spiceMustFlow : Card = { id: 'r_spice_must_flow', name: 'Spice Must Flow', type: 'main' };
+
+
 const allIntrigueCards: Card[] = [
   { id: 'i_ambush', name: 'Ambush', type: 'intrigue' },
   { id: 'i_secret', name: 'Secret Deal', type: 'intrigue' },
@@ -21,9 +30,15 @@ const allIntrigueCards: Card[] = [
 
 const allLeaderCards: Card[] = [
   { id: 'l_muaddib', name: 'Muad\'Dib', type: 'main' },
-  { id: 'l_jessica', name: 'Lady Jessica', type: 'main' },
-  { id: 'l_paul', name: 'Paul Atreides', type: 'main' },
+  { id: 'l_margot_fenry', name: 'Margot Fenry', type: 'main' },
+  { id: 'l_gurney_halleck', name: 'Gurney Halleck', type: 'main' },
   { id: 'l_duncan', name: 'Duncan Idaho', type: 'main' },
+];
+
+// fill this with all the conflict cards
+const allConflictCards: Card[] = [
+  { id: 'cf_1', name: 'Skirmish', type: 'conflict' },
+  
 ];
 
 export const DuneUprising: Game<GameState> = {
@@ -31,8 +46,32 @@ export const DuneUprising: Game<GameState> = {
   setup: ({ ctx }) => {
     const intrigueDeck = [...allIntrigueCards].sort(() => Math.random() - 0.5);
     const leaderDeck = [...allLeaderCards].sort(() => Math.random() - 0.5);
+    // TODO: one lv 1, five lv 2, all lv 3
+    const conflictDeck = [...allConflictCards].sort(() => Math.random() - 0.5);
+    const reserveDeck = [
+      ...Array.from({ length: 8 }, () => ({ ...prepareTheWay })),
+      ...Array.from({ length: 10 }, () => ({ ...spiceMustFlow }))
+    ];
+
+    const board: BoardState = {
+      'ImperialBasin': {
+        playerAgents: [],
+        cost: {
+          water: 0,
+          spice: 0,
+          solari: 0,
+        },
+        rewards: {
+          water: 0,
+          spice: 1,
+          solari: 0,
+        }
+      }
+    };
     const players: GameState['players'] = {};
     const garrisonedTroops: GameState['garrisonedTroops'] = {};
+    const currentRound = 1;
+    const shieldWallUp = true;
     for (let i = 0; i < ctx.numPlayers; i++) {
       const deck = [...allStartingCards].sort(() => Math.random() - 0.5);
       // randomize leader for now
@@ -45,12 +84,14 @@ export const DuneUprising: Game<GameState> = {
         water: 1,
         spice: 0,
         solari: 0,
-        spies: 3
+        spies: 3,
+        availableAgents: 2,
+        revealed: false,
       };
       garrisonedTroops[i.toString()] = 3;
     }
 
-    return { intrigueDeck, players, garrisonedTroops };
+    return { intrigueDeck, players, garrisonedTroops, conflictDeck, currentRound, shieldWallUp, board };
   },
 
   moves: {
@@ -81,14 +122,31 @@ export const DuneUprising: Game<GameState> = {
   },
 
   phases: {
-    Agent: {
+    plot: {
       moves: {
         drawCard: ({ G, ctx }) => {
           const playerId = ctx.currentPlayer;
           const card = G.players[playerId].deck.pop();
 
           if (!card) return;
+        },
+        placeAgent: ({ G, ctx, cardId, location }) => {
+          const playerId = ctx.currentPlayer;
+          const card = G.players[playerId].hand.find(card => card.id === cardId);
+
+          if (!card) return;
+
+          G.players[playerId].hand = G.players[playerId].hand.filter(card => card.id !== cardId);
+          G.players[playerId].availableAgents--;
+          G.board[location as string].playerAgents.push(playerId);
+        },
+        reveal: ({ G, ctx }) => {
+          const playerId = ctx.currentPlayer;
+          G.players[playerId].revealed = true;
         }
+      },
+      endIf: ({ G, ctx, random, ...plugins }) => {
+        return Object.values(G.players).every(player => player.revealed);
       }
     },
     Combat: {
@@ -109,7 +167,9 @@ export const DuneUprising: Game<GameState> = {
 
   // Ends the game if this returns anything.
   // The return value is available in `ctx.gameover`.
-  endIf: ({ G, ctx, random, ...plugins }) => null,
+  endIf: ({ G, ctx, random, ...plugins }) => {
+    return G.currentRound > 10;
+  },
 
   // Called at the end of the game.
   // `ctx.gameover` is available at this point.
